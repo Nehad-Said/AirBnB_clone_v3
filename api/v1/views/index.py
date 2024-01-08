c17d555c4c561a6ace26e87cf59c648dbed0e356
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-08 13:59:38 +0300

    added method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods = ['GET'])
 def status():
     """ This return the status Ok"""
     return jsonify({"status": "OK"})
