74f4472007777737f79186e759f676089b954a08
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-08 22:25:39 +0300

    api-count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,30 @@
 
 
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
-@app_views.route('/status', methods = ['GET'])
+@app_views.route('/status', methods=['GET'])
 def status():
     """ This return the status Ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'])
+def count():
+    """ Retrieve number of objects"""
+    return jsonify({
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+        })
