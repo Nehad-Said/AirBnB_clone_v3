5179bef796e2abf8987cf7b76396b19684cc25b1
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-13 12:56:06 +0300

    api status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""
-Module
-Flask Api module entry file (app).
-This is the main file of AirBnB project APIs
+"""creating api with flask"""
 
-"""
-from flask import Flask, jsonify
-from api.v1.views import app_views
+from flask import Flask
 from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 import os
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tear_down(exception=None):
-    """ Closes the database session on each request"""
+def tear_down(exception):
+    """closes the database after a req"""
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_error(e):
-    """ Return a json error"""
+def page_not_found(e):
+    """handle error"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    """Runs the app"""
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", "5000"))
-    app.run(port=port, host=host, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+
+    app.run(host=host, port=port, threaded=True)
